eba98376bf1bd58abe0d20bcf36e9221d80a50a4
sadhanrudresh <sadhanrudresh@gmail.com>
2017-10-05 18:41:41 +0530

    initial commit

---
+++ led.py
@@ -0,0 +1,12 @@
+# Python source file for prototype 11
+# 5th October
+
+import time
+import RPi.GPIO as GPIO       ## Import GPIO library
+GPIO.setmode(GPIO.BOARD)      ## Use board pin numbering
+GPIO.setup(11, GPIO.OUT)      ## Setup GPIO Pin 11 to OUT
+while True:
+	GPIO.output(20,True)  ## Turn on Led
+	time.sleep(1)         ## Wait for one second
+	GPIO.output(20,False) ## Turn off Led
+	time.sleep(1)         ## Wait for one second
---
+++ main.c
@@ -0,0 +1,9 @@
+// C source file for prototype 11
+// 5th October
+
+#include <stdio.h>
+
+int main() {
+	printf("hello world!\n");
+	return 0;
+}
