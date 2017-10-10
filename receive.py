c4d0a2d2c9672377b0fd830f8387b64de19f7ec8
Pranav S Bijapur <b.pranavshankar@gmail.com>
2017-10-11 01:44:31 +0530

    Update receive.py

--- receive.py
+++ receive.py
@@ -9,7 +9,7 @@ import os
 import json
 import time
 import psutil
-
+import hashlib
 
 
 # Shift to nodejs??
@@ -18,17 +18,28 @@ import psutil
 def cmp_versions(version_str_1, version_str_2):
     def normalize(v):
         return [int(x) for x in re.sub(r'(\.0+)*$','', v).split(".")]
-    return cmp(normalize(version1), normalize(version2))
-
-
+    if normalize(version_str_1) == normalize(version_str_2):
+        return 0
+    elif normalize(version_str_1) > normalize(version_str_2):
+        return 1
+    elif normalize(version_str_1) < normalize(version_str_2):
+        return -1
 
 #TODO TESTING
-validate_json
 def validate_json():
     new_json = json.load(open("/home/pi/code/main.json", 'r'))
     old_json = json.load(open("/home/pi/backup/main.json", 'r'))
     if cmp_versions(new_json["version"], old_json["version"]) < 0:
-        raise NotImplementedError
+        with open("/home/pi/data/error_log.txt", 'a') as fobj:
+            fobj.writelines([time.strftime("%b %d %Y %H:%M:%S", time.localtime(time.time())), "Error while checking versions\n"])
+    file_list = new_json["sources"]; hash_list = new_json["sha256"]
+    for source_file, hash_val in zip(file_list, hash_list):
+        fobj = open(source_file, 'r')
+        if hashlib.sha256(fobj.read()) != hash_val:
+            with open("/home/pi/data/error_log.txt", 'a') as fobj:
+                fobj.writelines([time.strftime("%b %d %Y %H:%M:%S   ", time.localtime(time.time())), "Error with sha256 values\n"])
+            break
+
 
 
 # directory structure planned:
@@ -41,9 +52,13 @@ def validate_json():
 
 try:
     os.chdir("/home/pi/code")
-except OSError, WindowsError, FileNotFoundError:
+except OSError as e:
     print("Error while changing directory")
-    with open("/home/pi/error_log.txt", 'a') as fobj:
+    with open("/home/pi/data/error_log.txt", 'a') as fobj:
+        fobj.writelines([time.strftime("%b %d %Y %H:%M:%S", time.localtime(time.time())), "Error while changing directory\n"])
+except FileNotFoundError as e:
+    print("Error while changing directory")
+    with open("/home/pi/data/error_log.txt", 'a') as fobj:
         fobj.writelines([time.strftime("%b %d %Y %H:%M:%S", time.localtime(time.time())), "Error while changing directory\n"])
 
 # TODO: unzip code.zip
@@ -52,13 +67,14 @@ except OSError, WindowsError, FileNotFoundError:
 # TODO: validation of json data
 retval = validate_json()
 
-for proc in psutil.process_iter():
-    pinfo = proc.as_dict(attrs=['pid', 'name'])
-    #procname = str(pinfo["name"])
-    ## TODO IMPORTANT - have to do mutex semaphore stuff here!!!
-    ## to make sure nothing important is happening
-    if procpid != str(os.getpid()):
-        proc.kill()
+def terminate_processes():
+    for proc in psutil.process_iter():
+        pinfo = proc.as_dict(attrs=['pid', 'name'])
+        #procname = str(pinfo["name"])
+        ## TODO IMPORTANT - have to do mutex semaphore stuff here!!!
+        ## to make sure nothing important is happening
+        if procpid != str(os.getpid()):
+            proc.kill()
 ## now replace the old files and reboot
 
 ## also have to take care of startup script to start runnning the code
