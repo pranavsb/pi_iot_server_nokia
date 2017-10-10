6e34dca2f17922119e0f05dc064e5ee785050f06
Pranav S Bijapur <b.pranavshankar@gmail.com>
2017-10-10 22:35:21 +0530

    Update receive.py
    Added functions to validate JSON data (not completely implemented) and
    check version numbers

--- receive.py
+++ receive.py
@@ -3,18 +3,41 @@
 # Python source file for prototype 11
 # 7th October
 
+import re
+import requests
 import os
 import json
 import time
 import psutil
 
+
+
 # Shift to nodejs??
 # No. Cuz python > nodejs
+
+def cmp_versions(version_str_1, version_str_2):
+    def normalize(v):
+        return [int(x) for x in re.sub(r'(\.0+)*$','', v).split(".")]
+    return cmp(normalize(version1), normalize(version2))
+
+
+
+#TODO TESTING
+validate_json
+def validate_json():
+    new_json = json.load(open("/home/pi/code/main.json", 'r'))
+    old_json = json.load(open("/home/pi/backup/main.json", 'r'))
+    if cmp_versions(new_json["version"], old_json["version"]) < 0:
+        raise NotImplementedError
+
+
 # directory structure planned:
 # TODO: configure rwx permissions for these:
 # /home/pi/data
 # /home/pi/code
-# /home/pi/error_log.txt
+# /home/pi/data/error_log.txt
+# /home/pi/backup
+# /home/pi/
 
 try:
     os.chdir("/home/pi/code")
@@ -22,6 +45,13 @@ except OSError, WindowsError, FileNotFoundError:
     print("Error while changing directory")
     with open("/home/pi/error_log.txt", 'a') as fobj:
         fobj.writelines([time.strftime("%b %d %Y %H:%M:%S", time.localtime(time.time())), "Error while changing directory\n"])
+
+# TODO: unzip code.zip
+
+
+# TODO: validation of json data
+retval = validate_json()
+
 for proc in psutil.process_iter():
     pinfo = proc.as_dict(attrs=['pid', 'name'])
     #procname = str(pinfo["name"])
