ce1a0a8da56812430aa86aad8066e8c922d41be5
Pranav S Bijapur <b.pranavshankar@gmail.com>
2017-10-11 12:48:59 +0530

    cleanup code
    Minor refactoring, function for process termination

--- receive.py
+++ receive.py
@@ -11,9 +11,8 @@ import time
 import psutil
 import hashlib
 
-
-# Shift to nodejs??
-# No. Cuz python > nodejs
+TIMEOUT = 5  # time in seconds to sleep
+URL = "http://192.168.43.57:3000/redcode"  # address of server
 
 def cmp_versions(version_str_1, version_str_2):
     def normalize(v):
@@ -25,25 +24,26 @@ def cmp_versions(version_str_1, version_str_2):
     elif normalize(version_str_1) < normalize(version_str_2):
         return -1
 
-#TODO TESTING
+def write_to_error_log(string_list):
+    with open("/home/pi/data/error_log.txt", 'a') as fobj:
+        fobj.writelines([time.strftime("%b %d %Y %H:%M:%S   ", time.localtime(time.time()))] + string_list)
+
 def validate_json():
     new_json = json.load(open("/home/pi/code/main.json", 'r'))
     old_json = json.load(open("/home/pi/backup/main.json", 'r'))
     if cmp_versions(new_json["version"], old_json["version"]) < 0:
-        with open("/home/pi/data/error_log.txt", 'a') as fobj:
-            fobj.writelines([time.strftime("%b %d %Y %H:%M:%S", time.localtime(time.time())), "Error while checking versions\n"])
+        write_to_error_log(["Error while checking versions\n"])
         return False
     file_list = new_json["sources"]; hash_list = new_json["sha256"]
     for source_file, hash_val in zip(file_list, hash_list):
         fobj = open(source_file, 'r')
         if hashlib.sha256(fobj.read()) != hash_val:
-            with open("/home/pi/data/error_log.txt", 'a') as fobj:
-                fobj.writelines([time.strftime("%b %d %Y %H:%M:%S   ", time.localtime(time.time())), "Error with sha256 values\n"])
+            write_to_error_log(["Error with sha256 values\n"])
             return False
     #TODO set timeout - write to file like config.txt?
+    time.sleep(TIMEOUT)
     #TODO verify key
-    with open("/home/pi/data/error_log.txt", 'a') as fobj:
-        fobj.writelines([time.strftime("%b %d %Y %H:%M:%S   ", time.localtime(time.time())), "JSON validated successfully\n"])
+    write_to_error_log(["JSON validated successfully\n"])
     return True
 
 # directory structure planned:
@@ -58,24 +58,48 @@ try:
     os.chdir("/home/pi/code")
 except OSError as e:
     print("Error while changing directory")
-    with open("/home/pi/data/error_log.txt", 'a') as fobj:
-        fobj.writelines([time.strftime("%b %d %Y %H:%M:%S", time.localtime(time.time())), "Error while changing directory\n"])
+    write_to_error_log(["Error while changing directory\n"])
 except FileNotFoundError as e:
-    print("Error while changing directory")
-    with open("/home/pi/data/error_log.txt", 'a') as fobj:
-        fobj.writelines([time.strftime("%b %d %Y %H:%M:%S", time.localtime(time.time())), "Error while changing directory\n"])
+    print("Error while finding files")
+    write_to_error_log(["Error while finding files\n"])
 
-# TODO: unzip code.zip
+# TODO code to ping server for code updates and other networking stuff
 
-
-# TODO: validation of json data
 retval = validate_json()
+if retval == True:
+    try:
+        os.chdir("/home/pi")
+        os.run("rm -r /home/pi/backup/")
+        os.run("mkdir /home/pi/backup")
+        os.run("cp -r /home/pi/code/. /home/pi/backup/.")
+        write_to_error_log(["Previous version backed up successfully\n"])
+        terminate_processes(TIMEOUT)
+        os.run("rm -r /home/pi/code")
+        os.system("unzip code.zip")
+    except OSError as e:
+        print("Error while changing directory")
+        write_to_error_log(["Error while changing directory\n"])
+    except FileNotFoundError as e:
+        print("Error while finding files")
+        write_to_error_log(["Error while finding files\n"])
+else:
+    write_to_error_log(["Error with JSON validation\n"])
 
-def terminate_processes():
+
+def terminate_processes(TIMEOUT):
     for proc in psutil.process_iter():
         pinfo = proc.as_dict(attrs=['pid', 'name'])
         #procname = str(pinfo["name"])
         ## TODO IMPORTANT - have to do mutex semaphore stuff here!!!
+        while True:
+            with open("/home/pi/code/running.txt") as fobj:
+                if int(fobj.read()) == 0:
+                    # safe to end processes
+                    break
+            time.sleep(TIMEOUT)
+            with open("/home/pi/data/error_log.txt", 'a') as fobj:
+                fobj.writelines([time.strftime("%b %d %Y %H:%M:%S", time.localtime(time.time())), "Error with terminating processes\n"])
+
         ## to make sure nothing important is happening
         if procpid != str(os.getpid()):
             proc.kill()
